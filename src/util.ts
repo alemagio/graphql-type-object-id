e62011d4d3baa4c4c3fcda6f815c75b9d835623a
Alessandro Magionami <alessandro.magionami@gmail.com>
2021-08-16 16:19:48 +0200

    fix: validation function to be more strict

--- src/util.ts
+++ src/util.ts
@@ -1,14 +1,28 @@
 import { ObjectId } from 'mongodb'
 
+export function isValidStringObjectId(value: string): boolean {
+  return (
+    typeof value === 'string' && ObjectId.isValid(value) && new ObjectId(value).toString() === value
+  )
+}
+
+export function isValidObjectId(value: ObjectId): boolean {
+  return (
+    value instanceof ObjectId &&
+    ObjectId.isValid(value) &&
+    new ObjectId(value).toString() === value.toString()
+  )
+}
+
 export function parseValue(value: string): ObjectId {
-  if (!ObjectId.isValid(value)) {
+  if (!isValidStringObjectId(value)) {
     throw new Error()
   }
   return new ObjectId(value)
 }
 
-export function serialize(value: string): string {
-  if (!ObjectId.isValid(value)) {
+export function serialize(value: ObjectId): string {
+  if (!isValidObjectId(value)) {
     throw new Error()
   }
   return value.toString()
